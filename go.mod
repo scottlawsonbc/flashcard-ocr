74376cc9e2c4cb9d4f83462bfb59204634b2d2e0
Scott Lawson <scott.lawson@parsable.com>
2019-12-24 16:15:12 -0700

    add test image and test program

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/scottlawsonbc/flashcard-ocr
 
 go 1.13
+
+require github.com/otiai10/gosseract v2.2.1+incompatible
--- main.go
+++ main.go
@@ -1,5 +1,16 @@
 package main
 
+import (
+	"fmt"
+	"github.com/otiai10/gosseract"
+)
+
 func main() {
-	print("hello, world!")
+	client := gosseract.NewClient()
+	defer client.Close()
+	client.SetImage("test.png")
+
+	text, _ := client.Text()
+	fmt.Println(text)
+	// Hello, World!
 }
